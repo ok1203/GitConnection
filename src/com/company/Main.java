31405bab5e9996d096c076835bca098fb15c07c3
User <omar.12200336@gmail.com>
2021-02-24 23:47:57 +0600

    just a try

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int x , y;
+        Scanner scan = new Scanner(System.in);
+        x = scan.nextInt();
+        y = scan.nextInt();
+        for(int i = 0 ; i < 3; i++){
+            for(int j = 0 ; j < 3 ; j++){
+                if(i == x && j ==y) {
+                    System.out.print("x ");
+                }
+                else{
+                    System.out.print("o ");
+                }
+            }
+            System.out.println("\n");
+        }
     }
 }
