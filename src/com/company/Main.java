d810121d7c4560ec19d7c00433790e076fec65d9
Kain991 <kainarberkin03012003@gmail.com>
2021-03-09 01:45:37 +0600

    Some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
+import java.awt.*;
+import java.sql.*;
 import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        new Menu();
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "postgres")) {
+            if (conn != null) {
+                System.out.println("Qosyldy");
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * from  scoreboard;");
+                int i = 1;
+                while (rs.next()) {
+                    System.out.println(rs.getInt("score") + "\t"
+                            + rs.getString("name"));
+                    i++;
+                }
+                System.out.println(i);
+            } else {
+                System.out.println("Ne rabotaet");
+            }
+        } catch (SQLException e) {
+            System.out.println("Qate");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         int counterofshots = 0;
         Field player1 = new Field();
         Field player2 = new Field();
@@ -10,16 +36,16 @@ public class Main {
         Field player4 = new Field();
         Scanner scan = new Scanner(System.in);
 
-        for (int i = 0; i < 6; i++){
-            int x , y;
+        for (int i = 0; i < 6; i++) {
+            int x, y;
             x = scan.nextInt();
             y = scan.nextInt();
             player1.locate_ship(x, y);
             player4.locate_ship(x, y);
         }
 
-        for (int i = 0; i < 6; i++){
-            int x , y;
+        for (int i = 0; i < 6; i++) {
+            int x, y;
             int a = 0, b = 8;
             x = a + (int) (Math.random() * b);
             y = a + (int) (Math.random() * b);
@@ -27,7 +53,7 @@ public class Main {
 
         }
 
-        while (counterofshots<64){
+        while (counterofshots < 64) {
             int countofrightshots1 = 0;
             int countofrightshots2 = 0;
             System.out.println("Your field:");
@@ -36,7 +62,7 @@ public class Main {
             player3.draw();
 
             System.out.println("Where will you shot(input coordinates):");
-            int x1 , y1;
+            int x1, y1;
             x1 = scan.nextInt();
             y1 = scan.nextInt();
             if (player2.Field[x1][y1] == "1") {
@@ -44,13 +70,13 @@ public class Main {
                 player3.Field[x1][y1] = "1";
                 countofrightshots1++;
 
-            }else{
+            } else {
                 System.out.println("you shooted wrong");
                 player3.Field[x1][y1] = "X";
             }
 
             System.out.println("Computer is making a shot");
-            int x2 , y2;
+            int x2, y2;
             int a = 0, b = 8;
             x2 = a + (int) (Math.random() * b);
             y2 = a + (int) (Math.random() * b);
@@ -59,12 +85,12 @@ public class Main {
                 System.out.println("computer shooted right");
                 player4.Field[x2][y2] = "X";
                 countofrightshots2++;
-            }else{
+            } else {
                 System.out.println("computer shooted wrong");
                 player4.Field[x2][y2] = "X";
             }
 
-            if (countofrightshots1 == 6 || countofrightshots2 == 6){
+            if (countofrightshots1 == 6 || countofrightshots2 == 6) {
                 break;
             }
 
@@ -85,4 +111,5 @@ public class Main {
             System.out.println("\n");
         }*/
     }
-}
+    }
+
