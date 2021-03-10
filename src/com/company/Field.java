4e415c7799659dec9d7d0b2043712bb32f5d55e8
User <omar.12200336@gmail.com>
2021-03-10 17:10:12 +0600

    just a try

--- src/com/company/Field.java
+++ src/com/company/Field.java
@@ -17,6 +17,129 @@ public class Field {
         Field[y][x] = "1";
     }
 
+    public int locate_ship2(int x, int y){
+        int option = 0;
+        System.out.println("How do you want to locate ships:");
+        System.out.println("1. --");
+        System.out.println("2. |");
+        System.out.println("   |");
+        option = scan.nextInt();
+
+        if (option == 2){
+            if (y == 7){
+                Field[y][x] = "1";
+                Field[y-1][x] = "1";
+            }else{
+                Field[y][x] = "1";
+                Field[y+1][x] = "1";
+            }
+        } else {
+            if (x == 7){
+                Field[y][x-1] = "1";
+                Field[y][x] = "1";
+            }else{
+                Field[y][x] = "1";
+                Field[y][x+1] = "1";
+            }
+        }
+    return option;
+
+    }
+
+    public void locate_ship2(int x, int y, int option){
+        if (option == 2){
+            if (y == 7){
+                Field[y][x] = "1";
+                Field[y-1][x] = "1";
+            }else{
+                Field[y][x] = "1";
+                Field[y+1][x] = "1";
+            }
+        } else {
+            if (x == 7){
+                Field[y][x-1] = "1";
+                Field[y][x] = "1";
+            }else{
+                Field[y][x] = "1";
+                Field[y][x+1] = "1";
+            }
+        }
+    }
+
+    public int locate_ship3(int x, int y){
+        int option = 0;
+        System.out.println("How do you want to locate ships:");
+        System.out.println("1. ---");
+        System.out.println("2. |");
+        System.out.println("   |");
+        System.out.println("   |");
+        option = scan.nextInt();
+
+        if (option == 2){
+            if (y == 7){
+                Field[y][x] = "1";
+                Field[y-1][x] = "1";
+                Field[y-2][x] = "1";
+            }else if (y == 6){
+                Field[y+1][x] = "1";
+                Field[y][x] = "1";
+                Field[y-1][x] = "1";
+            }else{
+                Field[y][x] = "1";
+                Field[y+1][x] = "1";
+                Field[y+2][x] = "1";
+            }
+        } else {
+            if (x == 7){
+                Field[y][x-2] = "1";
+                Field[y][x-1] = "1";
+                Field[y][x] = "1";
+            }else if (x == 6){
+                Field[y][x-1] = "1";
+                Field[y][x] = "1";
+                Field[y][x+1] = "1";
+            }else{
+                Field[y][x] = "1";
+                Field[y][x+1] = "1";
+                Field[y][x+2] = "1";
+            }
+        }
+        return option;
+
+    }
+
+    public void locate_ship3(int x, int y, int option){
+        if (option == 2){
+            if (y == 7){
+                Field[y][x] = "1";
+                Field[y-1][x] = "1";
+                Field[y-2][x] = "1";
+            }else if (y == 6){
+                Field[y+1][x] = "1";
+                Field[y][x] = "1";
+                Field[y-1][x] = "1";
+            }else{
+                Field[y][x] = "1";
+                Field[y+1][x] = "1";
+                Field[y+2][x] = "1";
+            }
+        } else {
+            if (x == 7){
+                Field[y][x-2] = "1";
+                Field[y][x-1] = "1";
+                Field[y][x] = "1";
+            }else if (x == 6){
+                Field[y][x-1] = "1";
+                Field[y][x] = "1";
+                Field[y][x+1] = "1";
+            }else{
+                Field[y][x] = "1";
+                Field[y][x+1] = "1";
+                Field[y][x+2] = "1";
+            }
+        }
+    }
+
     public void draw(){
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++){
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,57 +4,72 @@ import java.sql.*;
 import java.util.*;
 public class Main {
 
-    public static void main(String[] args) {
-        new Menu();
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "postgres")) {
-            if (conn != null) {
-                System.out.println("Qosyldy");
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * from  scoreboard;");
-                int i = 1;
-                while (rs.next()) {
-                    System.out.println(rs.getInt("score") + "\t"
-                            + rs.getString("name"));
-                    i++;
-                }
-                System.out.println(i);
-            } else {
-                System.out.println("Ne rabotaet");
-            }
-        } catch (SQLException e) {
-            System.out.println("Qate");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static int counterofshots = 64;
 
+    public static Scanner scan = new Scanner(System.in);
+    public static int option = 0;
 
-        int counterofshots = 64;
+    public static void start(){
         Field player1 = new Field();
         Field player2 = new Field();
         Field player3 = new Field();
         Field player4 = new Field();
-        Scanner scan = new Scanner(System.in);
+        System.out.println("Locate your ships");
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 1; i++) {
+            System.out.println("Input coordinates for 1 cell ship");
             int x, y;
             x = scan.nextInt();
             y = scan.nextInt();
             player1.locate_ship(x, y);
             player4.locate_ship(x, y);
         }
+        for (int i = 0; i < 1; i++) {
+            System.out.println("Input coordinates for 2 cell ship");
+            int x, y;
+            x = scan.nextInt();
+            y = scan.nextInt();
+            int option1 = player1.locate_ship2(x, y);
+            player4.locate_ship2(x, y, option1);
+        }
+        for (int i = 0; i < 1; i++){
+            System.out.println("Input coordinates for 3 cell ship");
+            int x, y;
+            x = scan.nextInt();
+            y = scan.nextInt();
+            int option1 = player1.locate_ship3(x, y);
+            player4.locate_ship3(x, y, option1);
+        }
 
-        for (int i = 0; i < 6; i++) {
+
+        for (int i = 0; i < 1; i++) {
             int x, y;
             int a = 0, b = 8;
             x = a + (int) (Math.random() * b);
             y = a + (int) (Math.random() * b);
             player2.locate_ship(x, y);
-
+        }
+        for (int i = 0; i < 1; i++){
+            int x, y;
+            int a = 0, b = 8;
+            x = a + (int) (Math.random() * b);
+            y = a + (int) (Math.random() * b);
+            int option1 = 1 + (int) (Math.random() * 2);
+            player2.locate_ship2(x, y, option1);
+        }
+        for (int i = 0; i < 1; i++){
+            int x, y;
+            int a = 0, b = 8;
+            x = a + (int) (Math.random() * b);
+            y = a + (int) (Math.random() * b);
+            int option1 = 1 + (int) (Math.random() * 2);
+            player2.locate_ship3(x, y, option1);
         }
 
         int countofrightshots1 = 0;
         int countofrightshots2 = 0;
+
+
 
         while (counterofshots >= 0) {
 
@@ -76,20 +91,27 @@ public class Main {
                 System.out.println("you shooted wrong");
                 player3.Field[x1][y1] = "X";
             }
-
             System.out.println("Computer is making a shot");
-            int x2, y2;
-            int a = 0, b = 8;
-            x2 = a + (int) (Math.random() * b);
-            y2 = a + (int) (Math.random() * b);
-            System.out.println(x2 + " " + y2);
-            if (player1.Field[x2][y2] == "1") {
-                System.out.println("computer shooted right");
-                player4.Field[x2][y2] = "X";
-                countofrightshots2++;
-            } else {
-                System.out.println("computer shooted wrong");
-                player4.Field[x2][y2] = "X";
+            while(true) {
+
+                int x2, y2;
+                int a = 0, b = 8;
+                x2 = a + (int) (Math.random() * b);
+                y2 = a + (int) (Math.random() * b);
+                if (player4.Field[x2][y2] == "1") {
+                    System.out.println(x2 + " " + y2);
+                    System.out.println("computer shooted right");
+                    player4.Field[x2][y2] = "X";
+                    countofrightshots2++;
+                    break;
+                } else if (player4.Field[x2][y2] == "X") {
+                    continue;
+                } else {
+                    System.out.println(x2 + " " + y2);
+                    System.out.println("computer shooted wrong");
+                    player4.Field[x2][y2] = "X";
+                    break;
+                }
             }
 
             if (countofrightshots1 == 6 || countofrightshots2 == 6) {
@@ -102,5 +124,147 @@ public class Main {
         System.out.println("Your score is " + counterofshots);
 
     }
+
+
+    public static void show(){
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "postgres")) {
+            if (conn != null) {
+                System.out.println("Qosyldy");
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * from  scoreboard;");
+                int i = 1;
+                while (rs.next()) {
+                    System.out.println(rs.getInt("score") + "\t"
+                            + rs.getString("name"));
+                    i++;
+                }
+                System.out.println(i);
+            } else {
+                System.out.println("Ne rabotaet");
+            }
+        } catch (SQLException e) {
+            System.out.println("Qate");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Menu();
+
+//        try (Connection conn = DriverManager.getConnection(
+//                "jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "postgres")) {
+//            if (conn != null) {
+//                System.out.println("Qosyldy");
+//                Statement stmt = conn.createStatement();
+//                ResultSet rs = stmt.executeQuery("SELECT * from  scoreboard;");
+//                int i = 1;
+//                while (rs.next()) {
+//                    System.out.println(rs.getInt("score") + "\t"
+//                            + rs.getString("name"));
+//                    i++;
+//                }
+//                System.out.println(i);
+//            } else {
+//                System.out.println("Ne rabotaet");
+//            }
+//        } catch (SQLException e) {
+//            System.out.println("Qate");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        int counterofshots = 64;
+//        Field player1 = new Field();
+//        Field player2 = new Field();
+//        Field player3 = new Field();
+//        Field player4 = new Field();
+//        Scanner scan = new Scanner(System.in);
+//        int option = 0;
+
+        while (option != 3){
+            System.out.println("Choose the option:");
+            System.out.println("1.Start the game");
+            System.out.println("2.Scoreboard");
+            System.out.println("3.Leave");
+            option = scan.nextInt();
+            if (option == 1){
+                start();
+            }
+            else if(option == 2){
+                show();
+            }
+        }
+//
+//        for (int i = 0; i < 6; i++) {
+//            int x, y;
+//            x = scan.nextInt();
+//            y = scan.nextInt();
+//            player1.locate_ship(x, y);
+//            player4.locate_ship(x, y);
+//        }
+//
+//        for (int i = 0; i < 6; i++) {
+//            int x, y;
+//            int a = 0, b = 8;
+//            x = a + (int) (Math.random() * b);
+//            y = a + (int) (Math.random() * b);
+//            player2.locate_ship(x, y);
+//
+//        }
+//
+//        int countofrightshots1 = 0;
+//        int countofrightshots2 = 0;
+//
+//
+//
+//        while (counterofshots >= 0) {
+//
+//            System.out.println("Your field:");
+//            player4.draw();
+//            System.out.println("Opponents field:");
+//            player3.draw();
+//
+//            System.out.println("Where will you shot(input coordinates):");
+//            int x1, y1;
+//            x1 = scan.nextInt();
+//            y1 = scan.nextInt();
+//            if (player2.Field[x1][y1] == "1") {
+//                System.out.println("you shooted right");
+//                player3.Field[x1][y1] = "1";
+//                countofrightshots1++;
+//
+//            } else {
+//                System.out.println("you shooted wrong");
+//                player3.Field[x1][y1] = "X";
+//            }
+//
+//            System.out.println("Computer is making a shot");
+//            int x2, y2;
+//            int a = 0, b = 8;
+//            x2 = a + (int) (Math.random() * b);
+//            y2 = a + (int) (Math.random() * b);
+//            System.out.println(x2 + " " + y2);
+//            if (player1.Field[x2][y2] == "1") {
+//                System.out.println("computer shooted right");
+//                player4.Field[x2][y2] = "X";
+//                countofrightshots2++;
+//            } else {
+//                System.out.println("computer shooted wrong");
+//                player4.Field[x2][y2] = "X";
+//            }
+//
+//            if (countofrightshots1 == 6 || countofrightshots2 == 6) {
+//                break;
+//            }
+//
+//            counterofshots--;
+//        }
+//
+//        System.out.println("Your score is " + counterofshots);
+
+        }
     }
 
